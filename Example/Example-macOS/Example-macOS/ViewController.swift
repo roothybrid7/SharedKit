058f24dc5304d2aeae702a1b2b9804201be68841
Satoshi Ohki <roothybrid7@gmail.com>
2017-01-12 20:18:09 +0900

    Added an example project

---
+++ Example/Example-macOS/Example-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example-macOS
+//
+//  Created by SATOSHI OHKI on 2017/01/12.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Example/Example-macOS/MyPlayground-macOS.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import SharedKit
+
+var str = "Hello, playground"
+let systemInfo = SystemInfo.systemInfo
+print(systemInfo.model, systemInfo.osName, systemInfo.osVersion)
