9e1f7e4477889e6086a126d1a0d4ca43b6f44f5f
Satoshi Ohki <roothybrid7@gmail.com>
2017-01-12 15:24:48 +0900

    Fixed build script

--- bin/archive.sh
+++ bin/archive.sh
@@ -11,5 +11,5 @@ filepath="dist/$(basename ${TRAVIS_BUILD_DIR})-${TRAVIS_TAG}.tar.gz"
 
 carthage build --configuration Release --no-skip-current
 mkdir -p dist
-tar zcf ${filepath} Carthage
+tar zcvf ${filepath} Carthage
 tar tvf ${filepath}
