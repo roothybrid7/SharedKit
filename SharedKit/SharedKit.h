bc38c737ea29888bf0279965570052df5c04e574
Satoshi Ohki <roothybrid7@gmail.com>
2017-01-12 16:35:05 +0900

    Removed SPM

---
+++ SharedKit/SharedKit.h
@@ -0,0 +1,19 @@
+//
+//  SharedKit.h
+//  SharedKit
+//
+//  Created by SATOSHI OHKI on 2017/01/12.
+//  Copyright © 2017年 Satoshi Ohki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedKit.
+FOUNDATION_EXPORT double SharedKitVersionNumber;
+
+//! Project version string for SharedKit.
+FOUNDATION_EXPORT const unsigned char SharedKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedKit/PublicHeader.h>
+
+
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import SharedKitTests
-
-XCTMain([
-     testCase(SharedKitTests.allTests),
-])
